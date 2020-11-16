e6ac163f69e3f72be93cca9769df3361ef95df60
Jimislimdrix <sethsjjones@gmail.com>
2020-11-16 13:23:15 -0600

    commit tree f21d98b33f91ab93f0125b88f0ff5bbdd05e70a6

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2 
+    last_element = array.pop 
+    array.join(", ") + ", and " +last_element
+  else 
+    array.join
+  end
 end
